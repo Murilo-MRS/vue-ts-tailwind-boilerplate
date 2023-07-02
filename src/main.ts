858e39988f57c152c8b11a8da91a473f4a006ebe
Murilo-MRS <murilosantana1993@gmail.com>
2023-07-02 19:35:06 -0300

    chore: add tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
-import './assets/main.css';
-
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import './style.css';
 
 import App from './App.vue';
 import router from './router';
